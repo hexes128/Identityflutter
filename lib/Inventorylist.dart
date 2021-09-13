60f2e8503920dcca65bb0bdcc2f2fc855a4b43a4
DESKTOP-N3JIIOP\hexes128 <hexes128@gmail.com>
2021-09-13 19:31:20 +0800

    TABBAR正常切換

--- lib/Inventorylist.dart
+++ lib/Inventorylist.dart
@@ -12,7 +12,8 @@ class InventoryList extends StatefulWidget {
   State<StatefulWidget> createState() => InventoryListState();
 }
 
-class InventoryListState extends State<InventoryList> {
+class InventoryListState extends State<InventoryList>
+    with TickerProviderStateMixin {
   Future<List<dynamic>> _callApi() async {
     var access_token = GV.tokenResponse.accessToken;
 
@@ -26,9 +27,7 @@ class InventoryListState extends State<InventoryList> {
           headers: {"Authorization": "Bearer $access_token"});
       if (response.statusCode == 200) {
         return jsonDecode(response.body);
-      } else {
-
-      }
+      } else {}
     } on Error catch (e) {
       throw Exception('123');
     }
@@ -38,98 +37,121 @@ class InventoryListState extends State<InventoryList> {
   void initState() {
     super.initState();
     PlaceList = _callApi();
+    tabController = TabController(length: 0, vsync: this);
   }
 
   Future<List<dynamic>> PlaceList;
   bool allChecked = false;
   List<dynamic> AreaList;
-
+  int Areaindex = 0;
   int Placeindex = 0;
   var status = ['正常', '借出', '報修', '停用'];
+  TabController tabController;
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<dynamic>>(
       future: PlaceList,
-
       builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
         if (snapshot.hasData) {
           AreaList = snapshot.data[Placeindex]['priorityList'];
-          AreaList.sort((a, b) => a['priorityNum'].compareTo(a['priorityNum']));
-
-          return DefaultTabController(
-            initialIndex: 0,
-            length: AreaList.length,
-            child: Scaffold(
-              appBar: AppBar(
-                title: Text('設備清單 ' + snapshot.data[Placeindex]['placeName']),
-                actions: [
-                  PopupMenuButton(
-                      onSelected: (int index) {
+          AreaList.sort((a, b) => a['priorityNum'].compareTo(b['priorityNum']));
+          tabController = TabController(
+              length: AreaList.length, vsync: this, initialIndex: Areaindex);
+          tabController.addListener(() {
+            if (tabController.indexIsChanging) {
+              setState(() {
+                Areaindex = tabController.index;
+              });
+            }
+          });
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(snapshot.data[Placeindex]['placeName'] +
+                  '(' +
+                  AreaList[Areaindex]['subArea'] +
+                  ')'),
+              actions: [
+                PopupMenuButton(
+                    onSelected: (int index) {
+                      if (Placeindex != index) {
                         setState(() {
                           Placeindex = index;
+                          tabController.animateTo(0);
                         });
-                      },
-                      icon: Icon(Icons.more_vert),
-                      itemBuilder: (BuildContext context) => snapshot.data
-                          .map((e) => PopupMenuItem(
-                                child: Text(e['placeName']),
-                                value: snapshot.data.indexOf(e),
-                              ))
-                          .toList()
-                      // <PopupMenuEntry>[
-                      //
-                      //   const PopupMenuItem(child: Text('Item A')),
-                      //
-                      // ],
-                      ),
-                ],
-              ),
-              body: TabBarView(
-                  children: AreaList.map((e) => ListView.builder(
-                        itemCount: e['fireitemList'].length,
-                        itemBuilder: (context, index) {
-                          var Fireitem = e['fireitemList'][index];
-                          return ListTile(
-                            leading: Checkbox(
-                              checkColor: Colors.white,
-                              value: Fireitem['ischeck'],
-                              onChanged: (bool value) {
-                                setState(() {
-                                  Fireitem['ischeck'] = value;
-                                });
-                              },
-                            ),
-                            title: Text(Fireitem['itemId'] +
-                                ' ' +
-                                Fireitem['itemName']),
-                            subtitle: Text(
-                                '當前狀態:' + status[Fireitem['presentStasus']]),
-                            onTap: () => {
+                      }
+                    },
+                    icon: Icon(Icons.sort),
+                    itemBuilder: (BuildContext context) => snapshot.data
+                        .map((e) => PopupMenuItem(
+                              child: Text(e['placeName']),
+                              value: snapshot.data.indexOf(e),
+                            ))
+                        .toList()),
+                PopupMenuButton(
+                  onSelected: (int index) {
+                    if (Placeindex != index) {
+                      setState(() {
+                        Placeindex = index;
+                        tabController.animateTo(0);
+                      });
+                    }
+                  },
+                  icon: Icon(Icons.camera_alt),
+                  itemBuilder: (BuildContext context) => [
+                    const PopupMenuItem(
+                      value: 0,
+                      child: Text('ON'),
+                    ),
+                    const PopupMenuItem(
+                      value: 1,
+                      child: Text('OFF'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            body: TabBarView(
+                controller: tabController,
+                children: AreaList.map((e) => ListView.builder(
+                      itemCount: e['fireitemList'].length,
+                      itemBuilder: (context, index) {
+                        var Fireitem = e['fireitemList'][index];
+                        return ListTile(
+                          leading: Checkbox(
+                            checkColor: Colors.white,
+                            value: Fireitem['ischeck'],
+                            onChanged: (bool value) {
                               setState(() {
-                                Fireitem['ischeck'] = !Fireitem['ischeck'];
-                              })
+                                Fireitem['ischeck'] = value;
+                              });
                             },
-                          );
-                        },
-                      )).toList()),
-              bottomNavigationBar: new Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Material(
-                    color: Theme.of(context).primaryColor,
-                    child: TabBar(
-                        indicatorColor: Colors.black,
-                        labelColor: Colors.black,
-                        unselectedLabelColor: Colors.white,
-                        isScrollable: true,
-                        tabs: AreaList.map((e) => Tab(text: e['subArea'] +' '+'(${e['fireitemList'].where((x)=>x['ischeck']==true).length}/${e['fireitemList'].length})'))
-                            .toList()),
-                  ),
-
-                ],
-              ),
+                          ),
+                          title: Text(
+                              Fireitem['itemId'] + ' ' + Fireitem['itemName']),
+                          subtitle:
+                              Text('當前狀態:' + status[Fireitem['presentStasus']]),
+                          onTap: () => {
+                            setState(() {
+                              Fireitem['ischeck'] = !Fireitem['ischeck'];
+                            })
+                          },
+                        );
+                      },
+                    )).toList()),
+            bottomNavigationBar: Material(
+              color: Theme.of(context).primaryColor,
+              child: TabBar(
+                  controller: tabController,
+                  indicatorColor: Colors.black,
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.white,
+                  isScrollable: true,
+                  tabs: AreaList.map((e) => Tab(
+                          text: e['subArea'] +
+                              ' ' +
+                              '(${e['fireitemList'].where((x) => x['ischeck'] == true).length}/${e['fireitemList'].length})'))
+                      .toList()),
             ),
           );
         } else if (snapshot.hasError) {
