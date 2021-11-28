5e70a34cfa09722f45749f1a2b3550e857f89593
DESKTOP-N3JIIOP\hexes128 <hexes128@gmail.com>
2021-11-28 18:00:01 +0800

    將新增設備改成可以新增多項

--- lib/additem.dart
+++ lib/additem.dart
@@ -61,11 +61,15 @@ class additemstate extends State<additemform> {
             "Authorization": "Bearer $access_token",
             'Content-Type': 'application/json; charset=UTF-8',
           },
-          body: jsonEncode(<String, dynamic>{
-            'ItemName': namecontroller.text,
-            'StoreId': Arealist[areacontroller.selectedItem]['storeId'],
-            'UserId': GV.userinfo.subject
-          }));
+          body: jsonEncode(
+              itemlist.map((e) {
+
+                return { 'ItemName':e.namecontroller.text,
+                  'StoreId':Arealist[e.scrollController.selectedItem]['storeId']};
+
+              }).toList()
+
+          ));
 
       if (response.statusCode == 200) {
         return response.body;
@@ -83,7 +87,7 @@ class additemstate extends State<additemform> {
 
   List<dynamic> PlaceList;
   List<dynamic> Arealist;
-
+  List<iteminput> itemlist = [];
   String Areavalue;
 
   int Placeindex = 0;
@@ -92,125 +96,195 @@ class additemstate extends State<additemform> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('新增設備'),
-      ),
-      body: Column(
-        children: [
-          Flexible(
-            child: Column(
-              children: [
-                TextField(
-                  controller: namecontroller,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), hintText: '設備名稱'),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                ),
-                FutureBuilder<List<dynamic>>(
-                  future: futureList,
-                  builder: (BuildContext context,
-                      AsyncSnapshot<List<dynamic>> snapshot) {
-                    if (snapshot.hasData) {
-                      PlaceList = snapshot.data;
-                      Arealist = PlaceList[Placeindex]['priorityList'];
+    return FutureBuilder<List<dynamic>>(
+      future: futureList,
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+        if (snapshot.hasData) {
+          PlaceList = snapshot.data;
+          Arealist = PlaceList[Placeindex]['priorityList'];
 
-                      return Row(children: [
-                        Expanded(
-                          child: Text('存放地'),
-                          flex: 1,
-                        ),
-                        Expanded(
-                          child: Center(
-                            child: CupertinoPicker(
-                              children: PlaceList.map((e) =>
-                                  Center(child: Text(e['placeName']))).toList(),
-                              itemExtent: 50,
-                              onSelectedItemChanged: (int index) {
-                                setState(() {
-                                  Placeindex = index;
-                                });
-                              },
-                            ),
-                          ),
-                          flex: 2,
-                        ),
-                        Expanded(
-                          child: Center(
-                            child: CupertinoPicker(
-                              scrollController: areacontroller,
-                              children: Arealist.map((e) => Center(
-                                  child: Text(e['subArea'] +
-                                      '(' +
-                                      '${Arealist.indexOf(e) + 1}' +
-                                      ')'))).toList(),
-                              itemExtent: 50,
-                              onSelectedItemChanged: (int index) {},
-                            ),
-                          ),
-                          flex: 2,
-                        ),
-                      ]);
-                    } else if (snapshot.hasError) {
-                      return Text('錯誤');
-                    } else {
-                      return Text('讀取資料中');
-                    }
-                  },
-                ),
-              ],
-            ),
-            flex: 2,
-          ),
-          Flexible(
-            child: Container(
-              margin: EdgeInsets.symmetric(vertical: 20.0),
-              height: 50,
-              width: 250,
-              decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-              child: FlatButton(
-                onPressed: () {
-                  if (namecontroller.text.trim().isEmpty) {
-                    Fluttertoast.showToast(
-                        msg: '名稱不可空白',
-                        toastLength: Toast.LENGTH_SHORT,
-                        gravity: ToastGravity.CENTER,
-                        timeInSecForIosWeb: 1,
-                        backgroundColor: Colors.red,
-                        textColor: Colors.white,
-                        fontSize: 16.0);
-                    return;
-                  }
-                  showDialog(
-                    context: context,
-                    builder: (context) => FutureProgressDialog(
-                        sendnewitem().then((value) {
-                          Fluttertoast.showToast(
-                              msg: '成功新增' + value + '筆紀錄',
-                              toastLength: Toast.LENGTH_SHORT,
-                              gravity: ToastGravity.CENTER,
-                              timeInSecForIosWeb: 1,
-                              backgroundColor: Colors.red,
-                              textColor: Colors.white,
-                              fontSize: 16.0);
-                          PlaceList[Placeindex]['todaysend'] = true;
-                        }),
-                        message: Text('資料處理中，請稍後')),
+          Arealist.sort((a, b) =>
+              a['priorityNum'].compareTo(b['priorityNum']));
+          return Scaffold(
+              appBar: AppBar(
+                  title: CupertinoPicker(
+
+                    children: PlaceList.map(
+                        (e) => Center(child: Text(e['placeName']))).toList(),
+                    itemExtent: 50,
+                    onSelectedItemChanged: (int index) {
+
+                      setState(() {
+                        Placeindex = index;
+                      });
+                    },
+                  ),
+                  actions: [
+                    IconButton(
+                        onPressed: () {
+                          setState(() {
+                            itemlist.add(iteminput());
+                          });
+                        },
+                        icon: Icon(Icons.add)),
+
+                    IconButton(
+                        onPressed: () {
+
+                          if (itemlist.length > 0) {
+                            if (itemlist.map((e) => e.namecontroller.text).where((e) => e.isEmpty).length > 0) {
+                              Fluttertoast.showToast(
+                                  msg: '尚有設備名稱未填寫',
+                                  toastLength: Toast.LENGTH_SHORT,
+                                  gravity: ToastGravity.CENTER,
+                                  timeInSecForIosWeb: 1,
+                                  backgroundColor: Colors.red,
+                                  textColor: Colors.white,
+                                  fontSize: 16.0);
+                              return;
+                            }
+                            showDialog<String>(
+                              context: context,
+                              builder: (BuildContext context) => AlertDialog(
+                                title: Text('確認以下設備?'),
+
+                                content: Container(
+                                    width: double.maxFinite,
+                                    child: ListView.builder(
+                                        itemCount: itemlist.length,
+                                        shrinkWrap: true,
+                                        itemBuilder: (BuildContext context, int index) {
+                                          return Card(
+                                            child: ListTile(
+                                              title:
+                                              Text(itemlist[index].namecontroller.text),
+                                            ),
+                                          );
+                                        })),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(
+                                        context,
+                                      );
+                                    },
+                                    child: Text('取消'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(
+                                        context,
+                                      );
+                                      showDialog(
+                                        context: context,
+                                        builder: (context) =>
+                                            FutureProgressDialog(
+                                                sendnewitem()
+                                                    .then((value) {
+                                                  Fluttertoast.showToast(
+                                                      msg: value,
+                                                      toastLength: Toast
+                                                          .LENGTH_SHORT,
+                                                      gravity: ToastGravity
+                                                          .CENTER,
+                                                      timeInSecForIosWeb: 1,
+                                                      backgroundColor:
+                                                      Colors.red,
+                                                      textColor:
+                                                      Colors.white,
+                                                      fontSize: 16.0);
+
+                                                }),
+                                                message: Text('資料處理中，請稍後')),
+                                      );
+
+                                    },
+                                    child: const Text('確定'),
+                                  ),
+                                ],
+                              ),
+                            );
+                          } else {
+                            Fluttertoast.showToast(
+                                msg: '尚未新增設備',
+                                toastLength: Toast.LENGTH_SHORT,
+                                gravity: ToastGravity.CENTER,
+                                timeInSecForIosWeb: 1,
+                                backgroundColor: Colors.red,
+                                textColor: Colors.white,
+                                fontSize: 16.0);
+                            return;
+                          }
+                        },
+                        icon: Icon(Icons.upload_rounded))
+                  ]),
+              body: ListView(
+                children: itemlist.map((controllers) {
+                  return
+
+                    Card(
+                    child: Row(children: [
+                      Expanded( child: TextField(
+                          controller: controllers.namecontroller,
+                          decoration: InputDecoration(
+                            fillColor: Colors.white,
+                            filled: true,
+                            hintText: '輸入設備名稱',
+                          )),flex: 7,),
+                      Expanded(child: CupertinoPicker(
+                        scrollController: controllers.scrollController,
+                        children: Arealist.map(
+                                (e) => Center(child: Text(e['subArea']))).toList(),
+                        itemExtent: 50,
+                        onSelectedItemChanged: (int index) {
+                          setState(() {
+
+                          });
+                        },
+                      ),flex: 2,),
+                      Expanded(child:    IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          setState(() {
+                            itemlist.remove(controllers);
+
+                          });
+                        },
+                      ),flex:1 ),
+
+
+
+                    ]),
                   );
-                },
-                child: Text(
-                  '送出',
-                  style: TextStyle(color: Colors.white, fontSize: 25),
-                ),
-              ),
-            ),
-            flex: 1,
-          )
-        ],
-      ),
+                }).toList(),
+              ));
+        } else if (snapshot.hasError) {
+          return Text('錯誤');
+        } else {
+          return Scaffold(
+            body: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text(
+                        '資料讀取中',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      CircularProgressIndicator(),
+                    ],
+                  ),
+                )),
+          );
+        }
+      },
     );
   }
+}
+class iteminput{
+
+ TextEditingController namecontroller = TextEditingController();
+ FixedExtentScrollController scrollController =FixedExtentScrollController() ;
+
 }
